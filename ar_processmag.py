3f1079495087d95e160fed4fe57b6ba3680e1e18
Sophie Murray <sophiemurray@gmail.com>
2017-10-17 12:28:01 +0100

    nan removal
    added removal of nans aswell as off limb zeroes in ar_processmag

--- ar_processmag.py
+++ ar_processmag.py
@@ -4,22 +4,24 @@
 cosmicthresh = 5000.  # A hard threshold for detecting cosmic rays
 
 import numpy as np
+from scipy import interpolate
 
 def ar_processmag(map):
     """...
     """
-    #already floats in numpy data array so skipped first line converting double to float
+    # already floats in numpy data array so skipped first line converting double to float
     imgsz = len(map.data) #not 1024 x 1024 like idl
-    #didnt load parameters - need to add to config file
-    #indextag didnt bother with
-    map.data = cosmicthresh_remove(map.data)
-    #Clean NaNs
-    map.data = nan_remove(map.data)
-    #Clean edge
-    map.data = edge_remove(map.data)
-    #Cosine map
+    # didnt load parameters - need to add to config file
+    # indextag didnt bother with
+    data = cosmicthresh_remove(map.data)
+    # Clean NaNs - Higgo used bilinear interpoaltion
+    data = nan_remove(data)
+    # Clean edge - make all pixels off limb equal to 0. -- commented out as done during nan removal above!
+    # map.data = edge_remove(map.data)
+    # Cosine map
     cosmap, limbmask = ar_cosmap(map)
-    #add property to map object
+    # Add property to map object
+    new_map = sunpy.map.Map(data, map.meta)
 
 def cosmicthresh_remove(data):
     """
@@ -29,32 +31,43 @@ def cosmicthresh_remove(data):
     wcosmic = np.where(data>cosmicthresh)
     ncosmic = len(wcosmic[0])
     print('Cosmic Ray Candidates Found: ', ncosmic)
-    wcx = wcosmic[0]
-    wcy = wcosmic[1]
-    neighbours = np.int_([-1, 0, 1, 1, 1, 0, -1, -1])
-    for i in range(0, ncosmic):
-        wcx_neighbours = wcx[i] + neighbours
-        wcy_neighbours = wcy[i] + neighbours
-        wc_logic = (3*np.std(data[wcx_neighbours, wcy_neighbours]))+np.mean(data[wcx_neighbours, wcy_neighbours])
-        if (data[wcx[i], wcy[i]] >= wc_logic):
-            data[wcx[i], wcy[i]] = np.mean(data[wcx_neighbours, wcy_neighbours])
-    return data
+    if (ncosmic == 0.):
+        return data
+    else:
+        wcx = wcosmic[0]
+        wcy = wcosmic[1]
+        neighbours = np.int_([-1, 0, 1, 1, 1, 0, -1, -1])
+        for i in range(0, ncosmic):
+            wcx_neighbours = wcx[i] + neighbours
+            wcy_neighbours = wcy[i] + neighbours
+            wc_logic = (3*np.std(data[wcx_neighbours, wcy_neighbours]))+np.mean(data[wcx_neighbours, wcy_neighbours])
+            if (data[wcx[i], wcy[i]] >= wc_logic):
+                data[wcx[i], wcy[i]] = np.mean(data[wcx_neighbours, wcy_neighbours])
+        return data
 
-
-def nan_remove(data):
+def nan_remove(array):
     """
     Clean NaNs
     Includes zero-value as 'missing'
     """
-    wnan = np.where(np.isnan(data))
-    data[wnan] = -9999.
-    fill_missing, data, -9999., 1. #IDL CODE - TO CHECK ON
-    return data
+    x = np.arange(0, array.shape[1])
+    y = np.arange(0, array.shape[0])
+    # mask invalid values
+    array = np.ma.masked_invalid(array)
+    xx, yy = np.meshgrid(x, y)
+    # get only the valid values
+    x1 = xx[~array.mask]
+    y1 = yy[~array.mask]
+    newarr = array[~array.mask]
+    GD1 = interpolate.griddata((x1, y1), newarr.ravel(),
+                               (xx, yy),
+                               method='cubic', fill_value=0.)
+    return GD1
 
 def edge_remove(data):
     """
     Get rid of crazy arbitrary values at the edge of the image
-
+    Basically set everything beyond the limb equal to zero
     """
     edgepix = data[0, 0]
     wblankpx = np.where(data == edgepix)
@@ -63,7 +76,13 @@ def edge_remove(data):
 
 def ar_cosmap(map):
     """
-    Get the cosine map and off-limb pixel map using WCS
+    Get the cosine map and off-limb pixel map using WCS.
+    ;Generate a map of the solar disk that is 1 at disk center and goes radially outward as the cos(angle to LOS) 
+    ;(= 2 at 60 degrees from LOS)
+    ;optionally output:	rrdeg = gives degrees from disk center
+    ;					wcs = wcs structure from input map file
+    ;					offlimb = map of 1=on-disk and 0=off-disk
+    ;					edgefudge = take off an extra half percent from the disk to get rid of limb effects
     """
     return cosmap, limbmask
 
