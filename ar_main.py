e00a489f8cf9e8811bf48d5c988f21c3d6eccf80
Sophie Murray <sophiemurray@gmail.com>
2017-10-24 16:22:58 +0100

    starting ar_detect

--- ar_main.py
+++ ar_main.py
@@ -1,9 +1,10 @@
 from ar_readmag import ar_readmag
 from ar_processmag import ar_processmag
-
+import sunpy
 
 # First load the data
-map = ar_readmag()
+#map = ar_readmag()
+map = sunpy.map.Map('/Users/sophie/data/smart/latest.fits')
 
 # Process it
 imgsz = len(map.data)
@@ -11,6 +12,11 @@ original_time = map.date
 thisdatafile = original_time.strftime('%Y%m%d_%H%M')
 
 # Now process magnetogram
-magproc cosmap, limbmask = ar_processmag(map)
+magproc, cosmap, limbmask = ar_processmag(map)
+
+#Create AR masks
+
+thissm = ar_detect(magproc, params = params, status = smartstatus, $
+                        cosmap = cosmap, limbmask = limbmask) ;prob dont need this?
 
 
