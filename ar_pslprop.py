14fda720269b4a675f65224db135f9a30a1c1c5c
Sophie Murray <sophiemurray@gmail.com>
2017-10-31 16:24:35 +0000

    ongoing

--- ar_pslprop.py
+++ ar_pslprop.py
@@ -29,6 +29,8 @@ projscl = 1.5 # F; the fractional increase/decrease in image size for the projec
 
 import numpy as np
 import sunpy.map
+from ar_detect import xyrcoord
+from ar_posprop import px2hc, hc2hg
 
 def ar_pslprop(map, thismask, dproj, projmaxscale):
     """.arpslstr={arid:0,psllength:0.,pslsglength:0.,pslcurvature:0d,rvalue:0.,wlsg:0.,bipolesep_mm:0.,bipolesep_px:0.,bipolesep_proj:0.}
@@ -45,37 +47,56 @@ def ar_pslprop(map, thismask, dproj, projmaxscale):
         # Take a sub-map around the AR
         thisdatmap = sunpy.map.Map(thisdat, map.meta)
         maskmap = sunpy.map.Map(thismask, map.meta)
-        xrange = ((np.min(np.where(thismask == 1)[1])-1, np.max(np.where(thismask == 1)[1])+1))
-        yrange = ((np.min(np.where(thismask == 1)[0])-1, np.max(np.where(thismask == 1)[0])+1))
+        #xrange = ((np.min(np.where(thismask == 1)[1])-1, np.max(np.where(thismask == 1)[1])+1))
+        #yrange = ((np.min(np.where(thismask == 1)[0])-1, np.max(np.where(thismask == 1)[0])+1))
         bottom_left_pixels = ((np.min(np.where(thismask == 1)[1])-1, np.min(np.where(thismask == 1)[0])-1))
+        #bl = pix_to_arc(map, bottom_left_pixels[0], bottom_left_pixels[1])
         top_right_pixels = ((np.max(np.where(thismask == 1)[1])+1, np.max(np.where(thismask == 1)[0])+1))
+        #tr = pix_to_arc(map, top_right_pixels[0], top_right_pixels[1])
+        submask = maskmap.submap(bottom_left_pixels * u.pixel, top_right_pixels * u.pixel)
+        submag = thisdatmap.submap(bottom_left_pixels * u.pixel, top_right_pixels * u.pixel)
+        #Convert to wcs structure??
+        # Determine the bipole separation properties
+        bipsepstr = ar_bipolesep(submag)
 
 
-def pix_to_arc(xrange, yrange):
-    xmap =
-    ymap =
-
-
-def get_map_xrange(map):
-    nx = map.data.shape[0]
-    xc = map.meta["crval1"] + map.meta["cdelt1"] * (((map.meta["naxis1"] + 1) / 2) - map.meta["crpix1"])
-    dx = map.meta['cdelt1']
-    xmin = np.min(xc-dx*(nx-1.)/2.)
-    xmax = np.max(xc+dx*(nx-1.)/2.)
-    return ((xmin, xmax))
-
-def get_map_yrange(map):
-    ny = map.data.shape[0]
+def ar_bipolesep(map):
+    """Determine the flux-weighted bipole separation distance between the pos and neg centroids
+    ;in degrees if a map is input, or in Px if only an image is input
+    """
+    image = map.data[::-1]
+    imgsz = image.shape
+    yy, xx, rr = xyrcoord(imgsz) #need to check this shit theres definitely something wrong - array flipped
+    imageneg = np.copy(image)
+    imageneg[np.where(image > 0.)] = 0.
+    imagepos = np.copy(image)
+    imagepos[np.where(image < 0.)] = 0.
+    pxpxloc = np.sum(xx * imagepos) / np.sum(imagepos)
+    nxpxloc = np.sum(xx * np.abs(imageneg)) / np.sum(np.abs(imageneg))
+    pypxloc = np.sum(yy * imagepos) / np.sum(imagepos)
+    nypxloc = np.sum(yy * np.abs(imageneg)) / np.sum(np.abs(imageneg))
+    pxsep = np.sum((pxpxloc-nxpxloc)^2.+(pypxloc-nypxloc)^2.)
+    # Now the map outputs
+    xc = map.meta["crval1"] + map.meta["cdelt1"]*(((map.meta["naxis1"] + 1)/2) - map.meta["crpix1"])
     yc = map.meta["crval2"] + map.meta["cdelt2"] * (((map.meta["naxis2"] + 1) / 2) - map.meta["crpix2"])
-    dy = map.meta['cdelt2']
-    ymin = np.min(yc-dy*(ny-1.)/2.)
-    ymax = np.max(yc+dy*(ny-1.)/2.)
-    return ((ymin, ymax))
+    phcxflx, phcyflx = px2hc(pxpxloc, pypxloc, map.meta['cdelt1'], map.meta['cdelt2'], xc, yc, imgsz)
+    phgxflx, phgyflx, carpxflx = hc2hg(map, phcxflx, phcyflx)
+    nhcxflx, nhcyflx = px2hc(nxpxloc, nypxloc, map.meta['cdelt1'], map.meta['cdelt2'], xc, yc, imgsz)
+    nhgxflx, nhgyflx, carnxflx = hc2hg(map, nhcxflx, nhcyflx)
+# sepstr.plon = phgxflx
+# sepstr.plat = phgyflx
+# sepstr.nlon = nhgxflx
+# sepstr.nlat = nhgyflx
+    gc_dist(((phgxflx, phgyflx)), ((nhgxflx, nhgyflx)))
 
-def
-    dxs = (max(xrange) - min(xrange)) / 2.
-    dys = (max(yrange) - min(yrange)) / 2.
-    xrange = [xrange[0] - dxs, xrange[0] + dxs]
-    yrange = [yrange[1] - dys, yrange[1] + dys]
+def gc_dist():
+
+
+def pix_to_arc(map, x, y):
+    """Convert pixel location to arcsecond location in map
+    """
+    arc_x = (x - (map.meta['crpix1'] - 1)) * map.meta['cdelt1'] + map.meta['crval1']
+    arc_y = (y - (map.meta['crpix2'] - 1)) * map.meta['cdelt2'] + map.meta['crval2']
+    return ((arc_x, arc_y))
 
 
