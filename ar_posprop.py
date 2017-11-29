edc3d43b6a46df138ba4a17ca1da2c0c7d94c125
Sophie Murray <sophiemurray@gmail.com>
2017-11-29 12:18:48 +0000

    completed
    swtiched map to thismap and added pandas dataframes

--- ar_posprop.py
+++ ar_posprop.py
@@ -31,56 +31,68 @@ from ar_detect import xyrcoord, ar_pxscale
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 from sunpy.coordinates import frames
+import pandas as pd
 
-
-def ar_posprop(map, mask, cosmap):
+def ar_posprop(thismap, mask, cosmap):
     """	blankstr={datafile:indatafile[0],arid:0,xcenbnd:0d, ycenbnd:0d, xcenflx:0d, ycenflx:0d, xcenarea:0d, ycenarea:0d, $
           hcxbnd:0d, hcybnd:0d, hcxflx:0d, hcyflx:0d, hcxarea:0d, hcyarea:0d, $
           hglonbnd:0d, hglatbnd:0d, hglonflx:0d, hglatflx:0d, hglonarea:0d, hglatarea:0d, $
           carlonbnd:0d,  carlonflx:0d,  carlonarea:0d, $
           xminbnd:0d, yminbnd:0d, xmaxbnd:0d, ymaxbnd:0d}
     """
-#    pxmmsq =  ar_pxscale(map, cmsqr=False, mmppx=False, cmppx=False)
-#    pxcmsq = ar_pxscale(map, cmsqr=True, mmppx=False, cmppx=False)
+#    pxmmsq =  ar_pxscale(thismap, cmsqr=False, mmppx=False, cmppx=False)
+#    pxcmsq = ar_pxscale(thismap, cmsqr=True, mmppx=False, cmppx=False)
     nmask = np.max(mask)
+    # Create dataframes
+    ardf = pd.DataFrame(columns = ['arid',
+                                 'xminbnd', 'yminbnd', 'xmaxbnd', 'ymaxbnd', 'xcenbnd', 'ycenbnd',
+                                 'xcenflx', 'ycenflx', 'xcenarea', 'ycenarea',
+                                 'hcxbnd', 'hcybnd', 'hcxflx', 'hcyflx', 'hcxarea', 'hcyarea',
+                                 'hglonbnd', 'hglatbnd', 'hglonflx', 'hglatflx', 'hglonarea', 'hglatarea',
+                                 'carlonbnd', 'carlonflx', 'carlonarea'])
+    posdf = ardf.copy()
+    negdf = ardf.copy()
     #For each AR...
     for i in range(1, np.int(nmask)+1):
         # Zero pixels outside detection boundary
-        thismask = np.copy(mask)
-        thismask[np.where(mask != i)] = 0.
-        thisdat = np.copy(map.data)
+        tmpmask = np.copy(mask)
+        tmpmask[np.where(mask != i)] = 0.
+        thisdat = np.copy(thismap.data)
         thisdat[np.where(mask != i)] = 0.
         thisabs = np.abs(thisdat)
         thisflx = thisabs*cosmap
         # Where are values within the detection boundary
-        thismask[np.where(mask == i)] = 1.
+        tmpmask[np.where(mask == i)] = 1.
 #        nothresh=0 & nopos=0 & noneg=0 & noposbnd=0 & nonegbnd=0
         # Where are the signed values
 #        wneg = np.where(thisdat < 0.)
 #        wpos = np.where(thisdat > 0.)
+        # Fill the position structure for whole detection
+        arstr = ar_posprop_findpos(i, thismap, np.where(mask == i), thisflx)
         # Same for positive and negative regions
-        posstr? = ar_posprop_findpos(map, np.where(thisdat > 0), thisflx)
-        negstr? = ar_posprop_findpos(map, np.where(thisdat < 0), thisflx)
-        # Fill the position structure for - whole detection
-        arstr? = ar_posprop_findpos(map, np.where(mask == i), thisflx)
-    # Fill position structure
-    ?? check differences in lists between posprop and findpos??
-    return arstr, posstr, negstr
+        posstr = ar_posprop_findpos(i, thismap, np.where(thisdat > 0), thisflx)
+        negstr = ar_posprop_findpos(i, thismap, np.where(thisdat < 0), thisflx)
+        # Fill position dataframes
+        ardf = ardf.append([arstr])
+        posdf = posdf.append([posstr])
+        negdf = negdf.append([negstr])
+    #TO DO: ?? check differences in lists between posprop and findpos??
+    return ardf#, posdf, negdf
 
-def ar_posprop_findpos(map, war, data):
+def ar_posprop_findpos(arid, thismap, war, data):
     """
     """
     data = np.abs(data)
-    date = map.meta['t_obs']
-    dx = map.meta['cdelt1']
-    dy = map.meta['cdelt2']
-    rsun = map.meta["rsun_obs"]
-    xc = map.meta["crval1"] + map.meta["cdelt1"]*(((map.meta["naxis1"] + 1)/2) - map.meta["crpix1"])
-    #xc = map.meta["crval1"] + map.meta["cdelt1"]*np.cos(map.meta["crota2"])*(((map.meta["naxis1"] + 1)/2) - map.meta["crpix1"]) - map.meta["cdelt2"]*np.sin(map.meta["crota2"])*(((map.meta["naxis2"] + 1)/2) - map.meta["crpix2"])
-    yc = map.meta["crval2"] + map.meta["cdelt2"] * (((map.meta["naxis2"] + 1) / 2) - map.meta["crpix2"])
-    #yc = map.meta["crval2"] + map.meta["cdelt1"]*np.sin(map.meta["crota2"])*(((map.meta["naxis1"] + 1)/2) - map.meta["crpix1"]) + map.meta["cdelt2"]*np.cos(map.meta["crota2"])*(((map.meta["naxis2"] + 1)/2) - map.meta["crpix2"])
+    date = thismap.meta['t_obs']
+    dx = thismap.meta['cdelt1']
+    dy = thismap.meta['cdelt2']
+    rsun = thismap.meta["rsun_obs"]
+    xc = thismap.meta["crval1"] + thismap.meta["cdelt1"]*(((thismap.meta["naxis1"] + 1)/2) - thismap.meta["crpix1"])
+    #xc = thismap.meta["crval1"] + thismap.meta["cdelt1"]*np.cos(thismap.meta["crota2"])*(((thismap.meta["naxis1"] + 1)/2) - thismap.meta["crpix1"]) - thismap.meta["cdelt2"]*np.sin(thismap.meta["crota2"])*(((thismap.meta["naxis2"] + 1)/2) - thismap.meta["crpix2"])
+    yc = thismap.meta["crval2"] + thismap.meta["cdelt2"] * (((thismap.meta["naxis2"] + 1) / 2) - thismap.meta["crpix2"])
+    #yc = thismap.meta["crval2"] + thismap.meta["cdelt1"]*np.sin(thismap.meta["crota2"])*(((thismap.meta["naxis1"] + 1)/2) - thismap.meta["crpix1"]) + thismap.meta["cdelt2"]*np.cos(thismap.meta["crota2"])*(((thismap.meta["naxis2"] + 1)/2) - thismap.meta["crpix2"])
     # get x and y indices
-    sz = map.data.shape
+    sz = thismap.data.shape
     xx, yy, rr = xyrcoord(sz)
     xwar = war[1]
     ywar = war[0]
@@ -95,43 +107,31 @@ def ar_posprop_findpos(map, war, data):
     ymaxbnd = yminmax[1]
     hcxbnd, hcybnd = px2hc(xcenbnd, ycenbnd, dx, dy, xc, yc, sz)
     #hgxbnd, hgybnd, carxbnd = hc2hg(hcxbnd, hcybnd, date, rsun, carx=True)
-    hgxbnd, hgybnd, carxbnd = hc2hg(map, hcxbnd, hcybnd)
+    hgxbnd, hgybnd, carxbnd = hc2hg(thismap, hcxbnd, hcybnd)
     # Determine the area weighted centroids
     xcenarea = np.sum(xwar * xx[war]) / np.sum(xx[war])
     ycenarea = np.sum(ywar * yy[war]) / np.sum(yy[war])
     hcxarea, hcyarea = px2hc(xcenarea, ycenarea, dx, dy, xc, yc, sz)
-    hgxarea, hgyarea, carxarea = hc2hg(map, hcxarea, hcyarea)
+    hgxarea, hgyarea, carxarea = hc2hg(thismap, hcxarea, hcyarea)
     # Determine the Flux weighted centroids
     xcenflx = np.sum(xwar * data[war]) / np.sum(data[war])
     ycenflx = np.sum(ywar * data[war]) / np.sum(data[war])
     hcxflx, hcyflx = px2hc(xcenflx, ycenflx, dx, dy, xc, yc, sz)
-    hgxflx, hgyflx, carxflx = hc2hg(map, hcxflx, hcyflx)
-# ;Fill structure
-# inoutstr.xminbnd=xminbnd
-# inoutstr.yminbnd=yminbnd
-# inoutstr.xmaxbnd=xmaxbnd
-# inoutstr.ymaxbnd=ymaxbnd
-# inoutstr.xcenbnd=xcenbnd
-# inoutstr.ycenbnd=ycenbnd
-# inoutstr.xcenflx=xcenflx
-# inoutstr.ycenflx=ycenflx
-# inoutstr.xcenarea=xcenarea
-# inoutstr.ycenarea=ycenarea
-# inoutstr.hcxbnd=hcxbnd
-# inoutstr.hcybnd=hcybnd
-# inoutstr.hcxflx=hcxflx
-# inoutstr.hcyflx=hcyflx
-# inoutstr.hcxarea=hcxarea
-# inoutstr.hcyarea=hcyarea
-# inoutstr.hglonbnd=hgxbnd
-# inoutstr.hglatbnd=hgybnd
-# inoutstr.hglonflx=hgxflx
-# inoutstr.hglatflx=hgyflx
-# inoutstr.hglonarea=hgxarea
-# inoutstr.hglatarea=hgyarea
-# inoutstr.carlonbnd=carxbnd
-# inoutstr.carlonflx=carxflx
-# inoutstr.carlonarea=carxarea
+    hgxflx, hgyflx, carxflx = hc2hg(thismap, hcxflx, hcyflx)
+    # Fill structure
+    posstr = {'arid': arid,
+              'xminbnd': xminbnd, 'yminbnd': yminbnd,
+              'xmaxbnd': xmaxbnd, 'ymaxbnd': ymaxbnd,
+              'xcenbnd': xcenbnd, 'ycenbnd': ycenbnd,
+              'xcenflx': xcenflx, 'ycenflx': ycenflx,
+              'xcenarea': xcenarea, 'ycenarea': ycenarea,
+              'hcxbnd': hcxbnd, 'hcybnd': hcybnd,
+              'hcxflx': hcxflx, 'hcyflx': hcyflx,
+              'hcxarea': hcxarea, 'hcyarea': hcyarea,
+              'hglonbnd': hgxbnd, 'hglatbnd': hgybnd,
+              'hglonflx': hgxflx, 'hglatflx': hgyflx,
+              'hglonarea': hgxarea, 'hglatarea': hgyarea,
+              'carlonbnd': carxbnd, 'carlonflx': carxflx, 'carlonarea': carxarea}
     return posstr
 
 
@@ -149,13 +149,13 @@ def px2hc(xpx, ypx, dx, dy, xc, yc, sz):
     hcy = ((ypx - (ys/2.))*dy) + yc
     return hcx, hcy
 
-def hc2hg(map, hcxbnd, hcybnd):
+def hc2hg(thismap, hcxbnd, hcybnd):
     """Convert heliocentric to heliographic coordinates using sunpy coordinates
     hc coords are in arcsecs
     hg coords are in degrees
     Also outputs Carrington longitude in degrees
     """
-    heliocentric = SkyCoord(hcxbnd, hcybnd, unit='arcsec', frame=map.coordinate_frame)
+    heliocentric = SkyCoord(hcxbnd, hcybnd, unit='arcsec', frame=thismap.coordinate_frame)
     heliographic = heliocentric.transform_to(frames.HeliographicStonyhurst)
     carrington = heliographic.transform_to(frames.HeliographicCarrington)
     return heliographic.lon.value, heliographic.lat.value, carrington.lon.value
