4c75f3da9c37f4d4b1929831a947b639c5e9ad90
Sophie Murray <sophiemurray@gmail.com>
2018-02-08 13:58:15 +0000

    output folder
    changed so it outputs folder and also got rid of unecessary plotting code

--- ar_readmag.py
+++ ar_readmag.py
@@ -45,10 +45,10 @@ def ar_readmag():
     link = get_link(config.get('input','fits_source'), int(config.get('input','strip_no')))
     data_dir = "".join([os.path.expanduser('~'), config.get('paths','data_dir')])
     fits = download(link,folder=data_dir)
-    ## Read file and plot it
-    thismap = plot_map(fits, data_dir)
+    ## Read file and output
+    thismap = sunpy.map.Map(fits)
     thismap.save(data_dir + 'latest.fits', filetype='auto')
-    return thismap
+    return thismap, data_dir
 
 def get_link(url,strip_no):
     """Standard URL grab, making sure the latest version is downloaded.
@@ -83,27 +83,6 @@ def web_request(url):
     web_file.add_header('Cache-Control', 'max-age=0')
     web_file = urllib.request.build_opener().open(web_file)
     return web_file
-
-def plot_map(fits, folder):
-    """Quickplot of HMI data just downloaded
-    Data is in thismap.data"""
-    thismap = sunpy.map.Map(fits)
-    ## Need to downsample if 4096x4096
-    if thismap.meta['naxis1']==4096:
-        thismap = thismap.resample(u.Quantity([1024, 1024], u.pixel))
-    ## Normalise
-    #thismap.plot_settings['norm'] = colors.Normalize(vmin=-500., vmax=500.)
-    #thismap.peek()
-    # fsz = 5
-    # f, (ax) = plt.subplots(1, figsize=[fsz, fsz])
-    # ax.set_xlabel('X (pixels)')
-    # ax.set_ylabel('Y (pixels)')
-    # ax.imshow(thismap.data)
-    # axpos = ax.get_position()
-    # dpi = thismap.data.shape[0] / (axpos.x1 - axpos.x0) / fsz
-    # plt.savefig(folder+'aia-figsave.png', dpi=dpi)
-    return thismap
-
 
 if __name__ == '__main__':
     ar_readmag()
