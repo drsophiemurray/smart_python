886d02e6051cd785d6f5b20960c887066526ad18
Sophie Murray <sophiemurray@gmail.com>
2018-02-08 10:57:52 +0000

    added title description
    description of code and its inputs added at beginning

--- ar_detect_core.py
+++ ar_detect_core.py
@@ -1,23 +1,24 @@
-# ;provide detections of more limited extent.
-# ;
-# ;STATUS =  -1 - the initialised value in the routine
-# ;			0 - skiped file because no detections were found by normal SMART run
-# ;			1 - skipped file because no PSL detections were found
-# ;			2 - no ridge/PSL mask overlay pixels found
-# ;			3 - no strong pixels found
-# ;			4 - no strong blobby pixels found
-# ;			5 - no stong-psl mask overlap found (using region grow)
-# ;			6 - no final core detections (failed multi-polarity test)
-# ;			7 - Core detections were found (lucky number seven!)
-# ;
-# ;NOTES:
-# ;	1. 	For each AR, id values go like 100,200,300
-# ;	2.	All positions with SMART mask pixels have 1. added to the mask value allowing one to subtract 1 and remove all SMART masks
-# ;	3. 	If the SMART mask pixels are touching a core detection the 2. is added, giving a value of 2. or 3.
-# ;	4. 	Thus to isolate only the core detections and have them increment by 1 (instead of 100), one would do: core_mask = ceil( ( ( combined_mask - 3. ) > 0 ) / 100. )
-# ;
+'''
+    SMART core detection code
+    ====================
+    Written by Sophie A. Murray, code originally developed by Paul Higgins (ar_detect_core.pro).
 
-#params
+    Developed under Python 3 and Sunpy 0.8.3
+    - Python 3.6.1 |Anaconda custom (x86_64)| (default, May 11 2017, 13:04:09)
+
+    Provides detections of more limited extent after the initial ar_detect
+
+    Inputs:
+    - thismap: Processed magnetogram
+    - smartmask: Output mask from ar_detect
+    - cmpmm: Number centimeters in a Mm.
+    - smoothphys: Physical gaussian smoothing HWHM in Mm. The radius of a characteristic supergranule
+    - smooththresh: Segmentation threshold used in processing magnetograms
+    (for 1kx1k gaussian smoothed; determined from plot_hmi_crosscal_resid.pro by comparing HMI and MDI detection masks)
+    - strongthresh: Segmentation threshold to detect strong field fragments which are then dilated to form the 'cores' of ARs.
+    - polethresh: Imbalanced fraction of total area covered by a given (+ or -) polarity (=abs(npos-nneg)/abs(npos+nneg)),
+    below which, the detection will be considered multipolar (core passes the flux balance test).
+'''
 
 from ar_detect import ar_pxscale, ar_grow
 from skimage.morphology import watershed
