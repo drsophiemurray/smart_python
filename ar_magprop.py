c31ebaf903c7eebd422dd461a79b78d0486d6dee
Sophie Murray <sophiemurray@gmail.com>
2018-02-08 12:45:51 +0000

    added config file. tidying
    added title description, added configparser, tidied comments

--- ar_magprop.py
+++ ar_magprop.py
@@ -1,25 +1,36 @@
-# ;Input a processed data map and mask
-# ;cosine correction for magnetic field values should already be done
-# ;The tot. area, pos. area, neg. area, and
-# ;	total, signed, fractional signed, negative, and positive flux
-# ;	are determined
-# ;
-# ;STATUS = output status array keyword (one status for each AR)
-# ;		0: initialised value
-# ;		7: Everything went swimmingly, and there should be valid magnetic properties for each AR
-# ;		1: No ARs were present in the mask! Should only happen if a blank array was read in
+'''
+    SMART magnetic property code
+    ============================
+    Written by Sophie A. Murray, code originally developed by Paul Higgins (ar_magprop.pro).
 
+    Developed under Python 3 and Sunpy 0.8.3
+    - Python 3.6.1 |Anaconda custom (x86_64)| (default, May 11 2017, 13:04:09)
+
+    Provides magnetic information of detected SMART regions, i.e.
+    - Total, positive, negative, area
+    - Total, postive, negative, signed, fractional signed, flux
+    - Max, min, mean, magnetic field strength
+
+    Inputs:
+    - inmap: Processed magnetogram
+    - inmask: Output SMART mask from ar_detect_core
+    - cosmap: Cosine map from processing
+    - magthresh: Secondary segmentation threshold (for MDI) used to find all flux fragments in an image
+'''
+
+from configparser import ConfigParser
 import numpy as np
 from ar_detect import ar_pxscale
 import pandas as pd
 
-magthresh= 350.0 # F; a secondary segmentation threshold (for MDI) used to find all flux fragments in an image
-
 def ar_magprop(inmap, inmask, cosmap):
-    """.	blankstr={datafile:indatafile[0],arid:0,areabnd:0., posareabnd:0., negareabnd:0., posarea:0., negarea:0., totarea:0., $
-          bmax:0d, bmin:0d, bmean:0d, $
-          totflx:0., imbflx:0., frcflx:0d, negflx:0., posflx:0.}
     """
+    Determine simple AR magnetic properties.
+    """
+    ## Load configuration file
+    config = ConfigParser()
+    config.read("config.ini")
+    ## Set up parameters and output dataframe
     pxmmsq =  ar_pxscale(inmap, cmsqr=False, mmppx=False, cmppx=False)
     pxcmsq = ar_pxscale(inmap, cmsqr=True, mmppx=False, cmppx=False)
     nmask = np.max(inmask)
@@ -29,45 +40,44 @@ def ar_magprop(inmap, inmask, cosmap):
                                     'bmax', 'bmin', 'bmean',
                                     'posflx', 'negflx', 'totflx',
                                     'imbflx', 'frcflx'])
-
-    #For each AR...
+    ## For each AR...
     for i in range(1, np.int(nmask)+1):
-        # Zero pixels outside of detection boundary
+        ## Zero pixels outside of detection boundary
         thismask = np.copy(inmask)
         thismask[np.where(inmask != i)] = 0.
         thisdat = np.copy(inmap.data)
         thisdat[np.where(inmask != i)] = 0.
         thisabs = np.abs(thisdat)
-        # Where are values within the detection boundary
+        ## Where are values within the detection boundary
         thismask[np.where(inmask == i)] = 1.
-        # Where values above mag threshold
-        wthresh = np.where(thisabs >= magthresh)
-        # Where negative values above thresh
-        wneg = np.where((thisdat < 0) & (thisabs >= magthresh))
-        # Where positive values above thresh
-        wpos = np.where((thisdat > 0) & (thisabs >= magthresh))
-        # Where negative values in boundary
+        ## Where values above mag threshold
+        wthresh = np.where(thisabs >= np.float(config.get('detection', 'magthresh')))
+        ## Where negative values above thresh
+        wneg = np.where((thisdat < 0) & (thisabs >= np.float(config.get('detection', 'magthresh'))))
+        ## Where positive values above thresh
+        wpos = np.where((thisdat > 0) & (thisabs >= np.float(config.get('detection', 'magthresh'))))
+        ## Where negative values in boundary
         wnegbnd = np.where(thisdat < 0)
-        # Where positive values in boundary
+        ## Where positive values in boundary
         wposbnd = np.where(thisdat > 0)
-        # Magnetic moments calculated for values within boundary [G]
+        ## Magnetic moments calculated for values within boundary [G]
         bmax = np.max(thisdat[np.where(inmask == i)])
         bmin = np.min(thisdat[np.where(inmask == i)])
         bmean = np.mean(thisdat[np.where(inmask == i)])
-        # Area of detection boundary [Mm^2]
+        ## Area of detection boundary [Mm^2]
         areabnd = np.sum(cosmap * thismask * pxmmsq)
         posareabnd = np.sum(cosmap[wposbnd] * thismask[wposbnd] * pxmmsq)
         negareabnd = np.sum(cosmap[wnegbnd] * thismask[wnegbnd] * pxmmsq)
         posarea = np.sum(cosmap[wpos] * thismask[wpos] * pxmmsq)
         negarea = np.sum(cosmap[wneg] * thismask[wneg] * pxmmsq)
         totarea = np.sum(cosmap[wthresh] * thismask[wthresh] * pxmmsq)
-        # Flux measurements [Mx = G cm^2]
+        ## Flux measurements [Mx = G cm^2]
         totflx = np.sum(cosmap * thismask * pxcmsq * thisabs)
         imbflx = np.sum(cosmap * thismask * pxcmsq * thisdat)
         negflx = np.sum(cosmap[wneg] * thismask[wneg] * pxcmsq * thisabs[wneg])
         posflx = np.sum(cosmap[wpos] * thismask[wpos] * pxcmsq * thisabs[wpos])
         frcflx = (posflx - negflx) / totflx
-        # Add to dataframe
+        ## Add to dataframe
         magdf = magdf.append([{'arid': i,
                              'areabnd': areabnd, 'posareanbnd': posareabnd, 'negareabnd': negareabnd,
                              'posarea': posarea, 'negarea': negarea, 'totarea': totarea,
@@ -75,5 +85,3 @@ def ar_magprop(inmap, inmask, cosmap):
                              'posflx': posflx, 'negflx': negflx, 'totflx': totflx,
                              'imbflx': imbflx, 'frcflx': frcflx}], ignore_index=True)
     return magdf
-
-
