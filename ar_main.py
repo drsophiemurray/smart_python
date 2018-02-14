911517b21ae6c3579dad9ef3880ad55dfc77e723
Sophie Murray <sophiemurray@gmail.com>
2018-02-14 09:28:23 +0000

    vis and json changes
    edited plotting routine to use rotated input fits file (added rotation to this part of code). improved json output including beautify. printed runtime in seconds. removed stuff done on to-do list

--- ar_main.py
+++ ar_main.py
@@ -14,10 +14,7 @@
     - Visualise output for SolarMonitor.org
 
     Notes:
-    - sunpy.wcs has been deprecated and needs to be replaced in ar_processmag.py
-    - there has to be a better way for median filtering a.l.a filer_image.pro
-    - need to finish deprojection code
-    - need to fix the PSLs that still show up when they shouldnt anymore
+    - need to finish deprojection code in ar_pslprop.py
 '''
 
 
@@ -32,57 +29,83 @@ import astropy.units as u
 import sunpy.map
 import matplotlib.pylab as plt
 import pandas as pd
-
+import datetime
+import numpy as np
+import json
+import time
 
 if __name__ == "__main__":
-    ## First load the latest HMI data file
+    # First load the latest HMI data file
+    start_time = time.time()
 #    thismap, data_dir = ar_readmag()
     # Currently manually loading for testing purposes, rather than using automatic scraping code above.
-    data_dir = '/Users/sophie/data/smart/'
+    print('Loading fits file')
+    data_dir = '/Users/sophie/Dropbox/'#'/Users/sophie/data/smart/'
     thismap = sunpy.map.Map(data_dir + 'latest.fits')
-    #'/Users/sophie/data/smart/latest.fits'
-    #'/Users/sophie/sunpy/data/hmi_m_45s_2011_10_17_00_01_30_tai_magnetogram.fits'
-    #'/Users/sophie/Downloads/hmi.M_720s.20140921_120000_TAI.fits'
 
-    ## Need to downsample if 4096x4096 (generally shouldnt be if using near-real-time JSOC data
+    # Need to downsample if 4096x4096
+    # (generally shouldnt be if using near-real-time JSOC data)
     if thismap.meta['naxis1'] == 4096:
         thismap = thismap.resample(u.Quantity([1024, 1024], u.pixel))
         thismap.meta['naxis1'] = 1024
         thismap.meta['naxis2'] = 1024
 
-    ## Now process magnetogram
+    # Rotate map if necessary
+    if (thismap.meta['crota2'] >= 100.):
+        data = np.flip(inmap.data, 1)[::-1]
+        inmap = sunpy.map.Map(data, inmap.meta)
+        inmap.meta['crota2'] = 0.
+
+    # Now process magnetogram
+    print('Processing data')
     magproc, cosmap, limbmask = ar_processmag(thismap, medianfilter=False)
 
-    ## Create AR masks
+    # Create AR masks
+    print('Making core detections')
     thissm = ar_detect(magproc, limbmask)
     thisar, pslmap = ar_detect_core(magproc, thissm.data)
-#    thismask = thisar.data
+    print('SMART detections found: ', np.max(np.unique(thisar.data)))
 
-    ## Get properties
+    # Get properties
+    print('Calculating properties')
     posprop = ar_posprop(magproc, thisar.data, cosmap)
     magprop = ar_magprop(magproc, thisar.data, cosmap)
     pslprop = ar_pslprop(magproc, thisar.data, doproj=False, projmaxscale=1024)
 
-    ## Output to json
-#    out = {'meta': {'some': 'meta info'}}
-#    out['posprop'] = posprop
-#    out['magprop'] = magprop
-#    out['pslprop'] = pslprop
-    print(pd.io.json.dumps(out))
+    # Output to json
+    smartdate = thismap.date.strftime('%Y%m%d_%H%M')
+    out = {'meta': {'dateobs': smartdate,
+                    'dimension': thismap.dimensions[0].value,
+                    'instrument': thismap.instrument}}
+    out['posprop'] = posprop
+    out['magprop'] = magprop
+    out['pslprop'] = pslprop
+    out = pd.io.json.dumps(out, data_dir+smartdate+'_properties.json')
+    with open(data_dir+smartdate+'_properties.json', 'w') as outfile:
+        outfile.write(out)
+    # Beautifying it
+    with open(data_dir+smartdate+'_properties.json') as infile:
+        obj = json.load(infile)
+    with open(data_dir+smartdate+'_properties.json', 'w') as outfile:
+        json.dump(obj, outfile,
+                  sort_keys=True,
+                  indent=4, separators=(',', ': '))
 
-    ## Visualise
-    magproc.plot(vmin=-500, vmax=500)
-    magproc.draw_limb()
+    # Visualise
+    thismap.plot(vmin=-500, vmax=500)
+    thismap.draw_limb()
     plt.colorbar(label='B [G]')
     plt.contour(pslmap.data, origin='lower',
-                colors='yellow', linewidths=0.1,
+                colors='yellow', linewidths=0.5,
                 vmin=0., vmax=np.max(np.unique(thisar.data))+1)
     plt.contour(thisar.data>0., origin='lower',
                 colors='blue', linewidths=1.0,
                 vmin=0., vmax=np.max(np.unique(thisar.data))+1)
 #    thismap.draw_grid(grid_spacing=10 * u.deg)
-    plt.savefig(data_dir + 'smart_detections.png')
+    plt.savefig(data_dir+smartdate+'_detections.eps')
 
+    # How long did that take?
+    print('Runtime:', round(time.time() - start_time),'seconds')
 
 # ======================================
 # Some IDL fits files used for testing:
@@ -92,3 +115,5 @@ if __name__ == "__main__":
 #    thisar_idl = sunpy.map.Map('thisar.fits')
 #    thismaskmap_idl = sunpy.map.Map('thismaskmap.fits')
 #    thismask_idl = thismaskmap_idl.data
+#'/Users/sophie/sunpy/data/hmi_m_45s_2011_10_17_00_01_30_tai_magnetogram.fits'
+#'/Users/sophie/Downloads/hmi.M_720s.20140921_120000_TAI.fits'
