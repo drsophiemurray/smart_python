rotation fix
theres too many problems with hmi and roll angle so i put rotation back into ar_processmag as a default process. i also see that the sunpy rotation changes hmi size so for consistency of images really just need to run it anyway (unless start looking into sunpy routines!)

ar_processmag.py
@@ -45,9 +45,9 @@ def ar_processmag(inmap, medianfilter):
     config = ConfigParser()
     config.read("config.ini")
     ## Rotate
-#    inmap = inmap.rotate(angle=int(inmap.meta['crota2'])*u.deg)
-#    inmap = inmap.resample(u.Quantity([1024, 1024], u.pixel))
-#    inmap.meta['crota2'] = 0.
+    inmap = inmap.rotate(angle=int(inmap.meta['crota2'])*u.deg)
+    inmap = inmap.resample(u.Quantity([1024, 1024], u.pixel))
+    inmap.meta['crota2'] = 0.
     # I commented out above as it was just adding way too much limb noise, so instead doing a hacky way
     if (inmap.meta['crota2'] >= 100.):
         data = np.flip(inmap.data, 1)[::-1]